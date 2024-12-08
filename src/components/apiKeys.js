3a0a938a5d89abd7386a61720d86d7a33d6976bc
TheUdayTechie <udayb0733@gmail.com>
2024-12-08 21:43:47 +0530

    Fix syntax errors

--- src/components/apiKeys.js
+++ src/components/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: <Enter_your_API_key>,
+  key: "2f027cace8007ea2f6d43f9cef0e7084",
   base: "https://api.openweathermap.org/data/2.5/",
 };
