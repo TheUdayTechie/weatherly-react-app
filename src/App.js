7a97c9cf7c87cdbb9fab700b872e913e911e0863
TheUdayTechie <udayb0733@gmail.com>
2024-12-08 20:42:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./components/currentLocation";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Weather />
+      </div>
+    </>
+  );
+}
+
+export default App;
